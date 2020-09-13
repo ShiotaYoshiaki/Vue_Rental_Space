2da44abb4b99a113e90786a1960ef012cdf3e8cc
ShiotaYoshiaki <shiota.yosiaki.0904@gmail.com>
2020-09-13 11:35:22 +0900

    共通か対応

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,30 +1,15 @@
 <template>
   <div>
-    <Header></Header>
-    <router-view></router-view>
-     <router-link to="/" class="link" active-class="link--active" exact>Home</router-link>
-     <router-link to="/search" class="link" active-class="link--active" exact>Search</router-link>
-     <router-link to="/favorites" class="link" active-class="link--active" exact>Favorites</router-link>
-     <router-link to="/account" class="link" active-class="link--active" exact>Account</router-link>
+    <Main />
   </div>
 </template>
 
 <script>
-import Header from './components/header.vue';
+import Main from './components/ordinary/Main';
 
 export default {
   components: {
-    Header,
+    Main,
   }
 }
 </script>
-
-<style scoped>
-.link {
-  margin-right: 20px;
-}
-
-.link--active {
-  font-size: 20px;
-}
-</style>
---
+++ my-project/src/components/ordinary/Router.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <router-link to="/" class="link" active-class="link--active" exact>Home</router-link>
+    <router-link to="/search" class="link" active-class="link--active" exact>Search</router-link>
+    <router-link to="/favorites" class="link" active-class="link--active" exact
+      >Favorites</router-link
+    >
+    <router-link to="/account" class="link" active-class="link--active" exact>Account</router-link>
+  </div>
+</template>
+
+<style scoped>
+.link {
+  margin-right: 20px;
+}
+
+.link--active {
+  font-size: 20px;
+}
+</style>
